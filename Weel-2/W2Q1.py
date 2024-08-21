bc80c4f779316d9bce07a45f49fcd77964f88514
Tayyab Khan <164236551+justtayyabkhxn@users.noreply.github.com>
2024-08-21 12:11:45 +0530

    Delete Weel-2 directory

--- Weel-2/W2Q1.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter a number: "))
-list=[]
-while int(n>0) :
-    list.append(int(n%10))
-    n=int(n/10)
-print(list)
--- Weel-2/W2Q5.py
+++
@@ -1,6 +0,0 @@
-n=int(input("Enter a number: "))
-sum=0
-while int(n>0) :
-    sum+=int(n%10)
-    n=int(n/10)
-print(sum)
